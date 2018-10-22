b3c0e9a61f7809787bff194107bec33e0ed9c6c4
yuanxx <824264439@qq.com>
2018-10-22 17:27:29 +0800

    增加定时任务 ，每三天更新一次订阅号文章

---
+++ luckydeer-dispatch/src/main/java/cn/luckydeer/dispatch/task/movie/MovieAritcleTask.java
@@ -0,0 +1,29 @@
+package cn.luckydeer.dispatch.task.movie;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import cn.luckydeer.manager.movie.MovieManager;
+
+/**
+ * 电影每周自动更新文章
+ * 
+ * @author yuanxx
+ * @version $Id: MovieAritcleTask.java, v 0.1 2018年10月22日 下午4:58:04 yuanxx Exp $
+ */
+public class MovieAritcleTask {
+
+    private static final Log logger = LogFactory.getLog("LUCKYDEER-TASK-LOG");
+
+    private MovieManager     movieManager;
+
+    public void run() {
+        logger.info("开始自动生成 订阅号文章");
+        //movieManager.getAirtcleInfo();
+    }
+
+    public void setMovieManager(MovieManager movieManager) {
+        this.movieManager = movieManager;
+    }
+
+}
